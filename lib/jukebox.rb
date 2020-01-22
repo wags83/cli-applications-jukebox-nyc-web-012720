4ae8ae34ef0b2389f1a4c7b05ea5fd2bf73052fc
Greg Wagner <gpwagner@gmail.com>
2020-01-22 14:10:40 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,12 @@
 # Add your code here
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list (song_array)
+  song_array.each_with_index {|song, index|}
+  puts "#{index+1}. #{song}"

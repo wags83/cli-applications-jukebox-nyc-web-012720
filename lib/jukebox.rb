d3aa9d7be1c600601f64a6ae22db8844b6dee713
Greg Wagner <gpwagner@gmail.com>
2020-01-22 14:35:14 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -7,8 +7,8 @@ def help
   puts "- exit : exits this program"
 end
 
-def list (song_array)
-  song_array.each_with_index {|song, index|
+def list(songs)
+songs.each_with_index {|song, index|
   puts "#{index+1}. #{song}"
 }
 end
